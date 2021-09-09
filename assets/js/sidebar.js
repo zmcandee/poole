a7ef9d8545cce54dd5e772cd16a0df443585993b
Zach Candee <zmcandee@users.noreply.github.com>
2021-09-08 21:11:01 -0500

    Add sidebar from lanyon

---
+++ assets/js/sidebar.js
@@ -0,0 +1,17 @@
+(function(document) {
+	  const $ = s => document.querySelector(s);
+	  const toggle = $('#sidebar-toggle');
+	  const sidebar = $('#sidebar');
+	  const checkbox = $('#sidebar-checkbox');
+
+	  document.addEventListener('click', function(e) {
+		      var target = e.target;
+
+		      if(!checkbox.checked ||
+			             sidebar.contains(target) || 
+			             target === checkbox || 
+			             toggle.contains(target)) return;
+
+		      checkbox.checked = false;
+		    }, false);
+})(document);
